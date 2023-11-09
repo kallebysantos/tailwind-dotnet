9f476cfbec8c4ace03bfecb81ca0265d9730e7a8
kallebysantos <kalleby_santos@hotmail.com>
2023-11-09 20:26:55 +0000

    📦 NEW: Creating a Hosting Startup class

--- TailwindDotnet.Hosting/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TailwindDotnet.Hosting;
-
-public class Class1
-{
-
-}
---
+++ TailwindDotnet.Hosting/HostingStartup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(TailwindDotnet.Hosting.SpaHostingStartup))]
+
+namespace TailwindDotnet.Hosting;
+
+internal sealed class SpaHostingStartup : IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder)
+    {
+        Console.WriteLine("Starting...");
+
+        builder.ConfigureServices(services =>
+        {
+            Console.WriteLine("Configure...");
+        });
+    }
+}
