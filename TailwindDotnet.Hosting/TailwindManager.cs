4e4be36193ad31ce058f9d4713586043ab34dfbd
kallebysantos <kalleby_santos@hotmail.com>
2023-11-15 11:15:32 +0000

    📦 NEW: Using tailwind options on Tailwind Manager

--- TailwindDotnet.Hosting/TailwindManager.cs
+++ TailwindDotnet.Hosting/TailwindManager.cs
@@ -1,20 +1,24 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace TailwindDotnet.Hosting;
 
 public sealed class TailwindManager : IDisposable
 {
     private Process? _tailwindProcess;
+
     private bool _disposedValue;
 
-    private const string TAILWIND_EXECUTABLE_URL =
-        "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss";
     private readonly TailwindOptions _options;
 
-    public TailwindManager(TailwindOptions options)
+    private readonly ILogger<TailwindManager> _logger;
+
+    public TailwindManager(IOptions<TailwindOptions> options, ILogger<TailwindManager> logger)
     {
-        _options = options;
+        _options = options.Value;
+        _logger = logger;
     }
 
     public static async Task<string?> Download(string tailwindExecutableUrl, string tailwindExecutablePath)
@@ -100,28 +104,28 @@ public sealed class TailwindManager : IDisposable
         return $"{baseUrl}/tailwindcss-{platform}-{arch}{ext}";
     }
 
-    public async Task LaunchTailwindProcess()
+    public void LaunchTailwindProcess()
     {
+        var (_, ext) = GetTailwindExecutablePlatform();
+        var executableFilename = _options.ExecutablePath + ext;
+        var commandArguments = new string[]
+        {
+            $"--input {_options.InputCssFile}",
+            $"--output {_options.OutputCssFile}",
+            $"--config {_options.ConfigFile}",
+            _options.IsWatchEnabled ? "--watch" : string.Empty,
+        };
+
+        var arguments = string.Join(' ', commandArguments);
+
         try
         {
-            var (_, ext) = GetTailwindExecutablePlatform();
-            var executableFilename = _options.ExecutablePath + ext;
-            var commandArguments = new string[]
-            {
-                $"--input {_options.InputCssFile}",
-                $"--output {_options.OutputCssFile}",
-                _options.IsWatchEnabled ? "--watch" : string.Empty,
-                _options.IsWatchEnabled ? "--minify" : string.Empty,
-            };
-
-            var arguments = string.Join(' ', commandArguments);
-
             var info = new ProcessStartInfo(executableFilename, arguments)
             {
                 // Linux and Mac OS don't have the concept of launching a terminal process in a new window. On those cases the process will be launched in the same terminal window and will just print some output during the start phase of the app.
                 CreateNoWindow = false,
+                UseShellExecute = true,
                 WindowStyle = ProcessWindowStyle.Normal,
-                RedirectStandardOutput = true,
                 WorkingDirectory = Path.Combine(AppContext.BaseDirectory, _options.WorkingDirectory)
             };
 
@@ -131,126 +135,13 @@ public sealed class TailwindManager : IDisposable
             {
                 throw new Exception("");
             }
-
-            while (!_tailwindProcess.StandardOutput.EndOfStream)
-            {
-                var line = await _tailwindProcess.StandardOutput.ReadLineAsync();
-                Console.Write(line);
-            }
         }
-        catch (Exception exception)
+        catch (Exception ex)
         {
-            Console.WriteLine(exception);
-            // _logger.LogError(
-            //     exception,
-            //     $"Failed to launch the SPA development server '{_options.LaunchCommand}'."
-            // );
-        }
-    }
-
-    private void LaunchStopScriptWindows(int spaProcessId)
-    {
-        var stopScript =
-            $@"do{{
-  try
-  {{
-    $processId = Get-Process -PID {Environment.ProcessId} -ErrorAction Stop;
-  }}catch
-  {{
-    $processId = $null;
-  }}
-  Start-Sleep -Seconds 1;
-}}while($processId -ne $null);
-
-try
-{{
-  taskkill /T /F /PID {spaProcessId};
-}}
-catch
-{{
-}}";
-        var stopScriptInfo = new ProcessStartInfo(
-            "powershell.exe",
-            string.Join(" ", "-NoProfile", "-C", stopScript)
-        )
-        {
-            CreateNoWindow = true,
-            WorkingDirectory = Path.Combine(AppContext.BaseDirectory, "")
-        };
-
-        var stopProcess = Process.Start(stopScriptInfo);
-        if (stopProcess == null || stopProcess.HasExited)
-        {
-            // if (_logger.IsEnabled(LogLevel.Warning))
-            // {
-            //     _logger.LogWarning(
-            //         $"The SPA process shutdown script '{stopProcess?.Id}' failed to start. The SPA proxy might"
-            //             + $" remain open if the dotnet process is terminated ungracefully. Use the operating system commands to kill"
-            //             + $" the process tree for {spaProcessId}"
-            //     );
-            // }
-        }
-        else
-        {
-            // if (_logger.IsEnabled(LogLevel.Debug))
-            // {
-            //     _logger.LogDebug($"Watch process '{stopProcess}' started.");
-            // }
-        }
-    }
-
-    private void LaunchStopScriptMacOS(int spaProcessId)
-    {
-        var fileName = Guid.NewGuid().ToString("N") + ".sh";
-        var scriptPath = Path.Combine(AppContext.BaseDirectory, fileName);
-        var stopScript =
-            @$"function list_child_processes () {{
-    local ppid=$1;
-    local current_children=$(pgrep -P $ppid);
-    local local_child;
-    if [ $? -eq 0 ];
-    then
-        for current_child in $current_children
-        do
-          local_child=$current_child;
-          list_child_processes $local_child;
-          echo $local_child;
-        done;
-    else
-      return 0;
-    fi;
-}}
-
-ps {Environment.ProcessId};
-while [ $? -eq 0 ];
-do
-  sleep 1;
-  ps {Environment.ProcessId} > /dev/null;
-done;
-
-for child in $(list_child_processes {spaProcessId});
-do
-  echo killing $child;
-  kill -s KILL $child;
-done;
-rm {scriptPath};
-";
-        File.WriteAllText(scriptPath, stopScript.ReplaceLineEndings());
-
-        var stopScriptInfo = new ProcessStartInfo("/bin/bash", scriptPath)
-        {
-            CreateNoWindow = true,
-            WorkingDirectory = Path.Combine(AppContext.BaseDirectory, "")
-        };
-
-        var stopProcess = Process.Start(stopScriptInfo);
-        if (stopProcess == null || stopProcess.HasExited)
-        {
-            // _logger.LogWarning(
-            //     $"The SPA process shutdown script '{stopProcess?.Id}' failed to start. The SPA proxy might"
-            //         + $" remain open if the dotnet process is terminated ungracefully. Use the operating system commands to kill"
-            //         + $" the process tree for {spaProcessId}"
-            // );
+            _logger.LogError(
+                exception: ex,
+                message: $"Failed to launch the Tailwind process tailwindcss '{arguments}'."
+            );
         }
     }
 
@@ -266,7 +157,7 @@ rm {scriptPath};
         {
             if (disposing)
             {
-                // Nothing to do here since ther are no managed resources
+                // Nothing to do here since there are no managed resources
             }
 
             try
@@ -274,16 +165,10 @@ rm {scriptPath};
                 if (_tailwindProcess != null && !_tailwindProcess.HasExited)
                 {
                     // Review: Whether or not to do this at all. Turns out that if we try to kill the
-                    // npm.cmd/ps1 process that we start, even with this option we only stop this process
+                    // tailwind process that we start, even with this option we only stop this process
                     // and the service keeps running.
                     // Compared to performing Ctrl+C on the window or closing the window for the newly spawned
                     // process which seems to do the right thing.
-                    // Process.CloseMainWindow seems to do the right thing in this situation and is doable since
-                    // we now start a proxy every time.
-                    // We can't guarantee that we stop/cleanup the proxy on every situation (for example if someone)
-                    // kills this process in a "rude" way, but this gets 95% there.
-                    // For cases where the proxy is left open and where there might not be a "visible" window the recommendation
-                    // is to kill the process manually. (We will not fail, we will simply notify the proxy is "already" up.
                     if (!_tailwindProcess.CloseMainWindow())
                     {
                         _tailwindProcess.Kill(entireProcessTree: true);
--- TailwindDotnet.Hosting/TailwindOptions.cs
+++ TailwindDotnet.Hosting/TailwindOptions.cs
@@ -6,17 +6,12 @@ public class TailwindOptions
 
     public string OutputCssFile { get; set; } = "output.css";
 
-    public bool IsWatchEnabled { get; set; } = false;
+    public string ConfigFile { get; set; } = "tailwind.config.js";
 
-    public bool IsMinifyEnabled { get; set; } = false;
+    public bool IsWatchEnabled { get; set; } = false;
 
     public string WorkingDirectory { get; set; } = string.Empty;
 
     public string ExecutablePath { get; set; } =
         Path.Combine(AppContext.BaseDirectory, "tailwindcss");
-
-    public string Version { get; set; } = "latest";
-
-    public string BaseDownloadUrl { get; set; } =
-        "https://github.com/tailwindlabs/tailwindcss/releases";
 }
