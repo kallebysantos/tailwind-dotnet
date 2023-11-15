79151436ec42108c817c1b6002cbd6040647d89f
kallebysantos <kalleby_santos@hotmail.com>
2023-11-15 11:16:24 +0000

    📦 NEW: Launching tailwind process on start

---
+++ TailwindDotnet.Hosting/TailwindHostingStartup.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(TailwindDotnet.Hosting.TailwindHostingStartup))]
+
+namespace TailwindDotnet.Hosting;
+
+internal sealed class TailwindHostingStartup : IHostingStartup
+{
+    [UnconditionalSuppressMessage("Trimming", "IL2026", Justification = "Configuration object's public properties are preserved.")]
+    static void ConfigureOptions<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(IServiceCollection services, IConfigurationSection section)
+     where T : class
+    {
+        services.Configure<T>(section);
+    }
+
+    public void Configure(IWebHostBuilder builder)
+    {
+        var tailwindPropsFile = Path.Combine(AppContext.BaseDirectory, "tailwind.props.json");
+        if (!File.Exists(tailwindPropsFile))
+        {
+            return;
+        }
+
+
+        builder.ConfigureServices(services =>
+            {
+                Console.WriteLine(tailwindPropsFile);
+                var configuration = new ConfigurationBuilder().AddJsonFile(tailwindPropsFile).Build();
+
+                ConfigureOptions<TailwindOptions>(services, configuration.GetSection("TailwindProps"));
+
+                services.AddSingleton<TailwindManager>();
+                services.AddSingleton<IStartupFilter, TailwindStartupFilter>();
+            }
+        );
+    }
+}
---
+++ TailwindDotnet.Hosting/TailwindStartupFilter.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TailwindDotnet.Hosting;
+
+internal sealed class TailwindStartupFilter : IStartupFilter
+{
+    private readonly TailwindManager _tailwindManager;
+
+    public TailwindStartupFilter(TailwindManager tailwindManager)
+    {
+        _tailwindManager = tailwindManager ?? throw new ArgumentNullException(nameof(tailwindManager));
+    }
+
+    public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+    {
+        _tailwindManager.LaunchTailwindProcess();
+
+        return builder => next(builder);
+    }
+}
