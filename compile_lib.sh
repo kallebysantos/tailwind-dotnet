a2f1584e7e2971e29a3c783157d4e0e5bdc1a36f
Kalleby Santos <105971119+kallebysantos@users.noreply.github.com>
2025-03-14 15:18:25 +0000

    feat: Tailwind minified output on `Publish` (#3)
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * feat: generate minified css on publish
    
    * stamp: delete all tailwind related files on clean
    
    * stamp: git-ignoring generated `app.css` file
    
    * stamp: compile script using user path
    
    * stamp: update readme properties table

--- compile_lib.sh
+++ compile_lib.sh
@@ -1,1 +1,19 @@
-rm -R /home/kalleby/.nuget/packages/tailwind.hosting; dotnet remove examples/Blazor package Tailwind.Hosting; dotnet pack Tailwind.Hosting; dotnet add examples/Blazor package Tailwind.Hosting; dotnet run --project examples/Blazor
+rm -R ~/.nuget/packages/tailwind.hosting
+rm -R ~/.nuget/packages/tailwind.hosting.build
+rm -R ~/.nuget/packages/tailwind.hosting.cli
+
+rm -R ./examples/Blazor/bin/
+rm -R ./examples/Blazor/obj/
+rm -R ./nupkgs/*
+
+dotnet clean
+dotnet remove examples/Blazor package Tailwind.Hosting
+dotnet remove examples/Blazor package Tailwind.Hosting.Build
+
+dotnet build
+dotnet pack Tailwind.Hosting
+dotnet pack Tailwind.Hosting.Build
+dotnet pack Tailwind.Hosting.Cli
+
+dotnet add examples/Blazor package Tailwind.Hosting
+dotnet add examples/Blazor package Tailwind.Hosting.Build
